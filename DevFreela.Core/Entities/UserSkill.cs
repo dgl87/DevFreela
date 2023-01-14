4ff8cd6c5ebe347142554c3647024a0f8d0c17e1
Douglas <dglandre@gmail.com>
2023-01-13 23:02:48 -0300

    feat: Camada Core

---
+++ DevFreela.Core/Entities/UserSkill.cs
@@ -0,0 +1,13 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class UserSkill : BaseEntity
+    {
+        public UserSkill(int id, int idSkill)
+        {
+            Id = id;
+            IdSkill = idSkill;
+        }
+        public int Id { get; private set; }
+        public int IdSkill { get; private set; }
+    }
+}
